6d6d5a0f59971b80466faa80d0216f36538de061
Marek Piechut <marek.piechut@gmail.com>
2013-12-26 21:41:42 +0100

    enable listener only on top window (not in iframes)

--- injected.js
+++ injected.js
@@ -2,16 +2,18 @@ translateMe = {}
 
 translateMe.pages = {
 	listener: function(msg) {
-		if(msg.name === 'translate_selected') {
-			var text = translateMe.pages.getSelectedText();
-			var url = window.location.href;
-			var response = {text: text, url: url};
-			safari.self.tab.dispatchMessage('translate_text', response);
-		} else if(msg.name === 'show_translated' && !msg.shown)  {
-			var translation = msg.message;
-			log.debug("Showing translation: " + JSON.stringify(translation));			
-			translateMe.ui.showTranslated(translation);
-			msg.shown = true;
+		if (window.top === window) {
+			if(msg.name === 'translate_selected') {
+				var text = translateMe.pages.getSelectedText();
+				var url = window.location.href;
+				var response = {text: text, url: url};
+				safari.self.tab.dispatchMessage('translate_text', response);
+			} else if(msg.name === 'show_translated' && !msg.shown)  {
+				var translation = msg.message;
+				log.debug("Showing translation: " + JSON.stringify(translation));			
+				translateMe.ui.showTranslated(translation);
+				msg.shown = true;
+			}
 		}
 	},
 	
