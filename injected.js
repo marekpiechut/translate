5cc23994884a00399a29eda2537f816049419662
Marek Piechut <marek.piechut@gmail.com>
2014-01-06 22:03:29 +0100

    refactor page listeners to module

--- injected.js
+++ injected.js
@@ -1,26 +1,37 @@
 translateMe = {}
 
-translateMe.pages = {
-	listener: function(msg) {
-		if (window.top === window) {
-			if(msg.name === 'translate_selected') {
-				var text = translateMe.pages.getSelectedText();
-				var url = window.location.href;
-				var response = {text: text, url: url};
-				safari.self.tab.dispatchMessage('translate_text', response);
-			} else if(msg.name === 'show_translated' && !msg.shown)  {
+translateMe.pages = function(){
+	var listeners = {
+		translate_selected: function(msg) {
+			var text = translateMe.pages.getSelectedText();
+			var url = window.location.href;
+			var response = {text: text, url: url};
+			safari.self.tab.dispatchMessage('translate_text', response);
+		},
+		
+		show_translated: function(msg) {
+			if(!msg.shown) {
 				var translation = msg.message;
 				log.debug("Showing translation: " + JSON.stringify(translation));			
 				translateMe.ui.showTranslated(translation);
 				msg.shown = true;
 			}
 		}
-	},
-	
-	getSelectedText: function() {
-		var text = window.getSelection().toString();
-		return text;
 	}
-}
+	
+	return {
+		listener: function(msg) {
+			if (window.top === window) {
+				var fn = listeners[msg.name] || function(msg) {log.error("no page listener for name: " + msg.name)};
+				fn(msg);
+			}
+		},
+	
+		getSelectedText: function() {
+			var text = window.getSelection().toString();
+			return text;
+		}
+	}
+}();
 
 safari.self.addEventListener('message', translateMe.pages.listener, false);
